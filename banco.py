dc15836415ab3c0761d15cc7839edea955c374ab
Deus <maickon0729@gmail.com>
2026-05-04 15:10:00 -0300

    Adicionando criação automatica da tabela no banco.py, para funcionar em qualquer lugar, pycharm,vscode e etc

--- banco.py
+++ banco.py
@@ -3,4 +3,15 @@ import sqlite3
 def conectar():
     conexao = sqlite3.connect("MeuBanco.db")
     cursor = conexao.cursor()
+    
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS alunos (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT,
+            idade INTEGER,
+            plano TEXT
+        )
+    """)
+    conexao.commit()
+    
     return conexao, cursor
