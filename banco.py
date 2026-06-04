497d4a7d51c441c0652c92128e04e3b14555bb76
Maickon <maickon0729@gmail.com>
2026-06-04 17:07:14 -0300

    Mudança do sqlite ao mysql

--- banco.py
+++ banco.py
@@ -1,17 +1,15 @@
-import sqlite3
+import mysql.connector
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 def conectar():
-    conexao = sqlite3.connect("MeuBanco.db")
+    conexao = mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
+    )
     cursor = conexao.cursor()
-    
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS alunos (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            nome TEXT,
-            idade INTEGER,
-            plano TEXT
-        )
-    """)
-    conexao.commit()
-    
     return conexao, cursor
